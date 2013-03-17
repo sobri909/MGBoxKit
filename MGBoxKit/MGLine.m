378c3c4aedf350521c43c0c1c5b41d19b8a66ae8
Matt Greenfield <matt@bigpaua.com>
2013-03-17 22:01:29 +0700

    NSAttributedString bug workaround label width padding also for single line labels #49

--- MGBoxKit/MGLine.m
+++ MGBoxKit/MGLine.m
@@ -866,7 +866,7 @@
     CGSize maxSize = (CGSize){FLT_MAX, 0};
     CGSize size = [label.attributedText boundingRectWithSize:maxSize
         options:NSStringDrawingUsesLineFragmentOrigin context:nil].size;
-    size.width = ceilf(size.width);
+    size.width = ceilf(size.width) + 2;
     size.height = ceilf(size.height);
 
     // for auto resizing margin sanity, make height odd/even match with self
