d827fa91cdec0a918c12a1b1ceb7577ec7ca5c44
Matt Greenfield <matt@bigpaua.com>
2014-06-09 20:27:57 +0700

    use new MGEvents.h from MGEvents pod

--- MGBoxKit/MGBase.h
+++ MGBoxKit/MGBase.h
@@ -4,7 +4,6 @@
 
 #define CLAMP(x,low,high) (((x) > (high)) ? (high) : (((x) < (low)) ? (low) : (x)))
 
+#import <MGEvents/MGEvents.h>
 #import "UIView+MGEasyFrame.h"
-#import "NSObject+MGEvents.h"
-#import "UIControl+MGEvents.h"
 #import "UIResponder+FirstResponder.h"
